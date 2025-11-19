fa23ccac2182cd877f8533127b3465dfc8ce507d
Sanjh Dey Sarker <sanjhdeysarker@gmail.com>
2025-11-19 14:40:20 +0530

    create role-middleware

---
+++ backend/src/routes/flightRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  addFlight,
+  getAllFlights,
+  searchFlights,
+  getFlightById,
+  updateFlight,
+  deleteFlight
+} = require("../controllers/flightController");
+
+const protect = require("../middleware/authMiddleware");  // JWT
+const admin = require("../middleware/roleMiddleware");     // Admin check
+
+// Admin Operations
+router.post("/", protect, admin, addFlight);
+router.put("/:id", protect, admin, updateFlight);
+router.delete("/:id", protect, admin, deleteFlight);
+
+// Public Routes
+router.get("/", getAllFlights);
+router.get("/search", searchFlights);
+router.get("/:id", getFlightById);
+
+module.exports = router;

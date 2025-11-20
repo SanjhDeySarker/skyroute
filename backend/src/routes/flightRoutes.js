05a37c532284db6f81886089a148ae2a510daf24
Sanjh Dey Sarker <sanjhdeysarker@gmail.com>
2025-11-21 01:18:27 +0530

    flight-route-fix-issue

--- backend/src/routes/flightRoutes.js
+++ backend/src/routes/flightRoutes.js
@@ -12,3 +12,15 @@ const {
 
 const protect = require("../middleware/authMiddleware");
 const admin = require("../middleware/roleMiddleware");
+
+// Admin Routes
+router.post("/", protect, admin, addFlight);
+router.put("/:id", protect, admin, updateFlight);
+router.delete("/:id", protect, admin, deleteFlight);
+
+// Public Routes
+router.get("/", getAllFlights);
+router.get("/search", searchFlights);
+router.get("/:id", getFlightById);
+
+module.exports = router;

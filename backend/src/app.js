7765f117e2c17da66d58cec928958435f6aa8814
Sanjh Dey Sarker <sanjhdeysarker@gmail.com>
2025-11-24 02:33:22 +0530

    app-update

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
+// Import Routes
 const authRoutes = require("./routes/authRoutes");
-const flightRoutes = require("./routes/flightRoutes");  
+const flightRoutes = require("./routes/flightRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
 
 const app = express();
 
@@ -12,10 +16,13 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/flights", flightRoutes);  
+app.use("/api/flights", flightRoutes);
+app.use("/api/bookings", bookingRoutes);
+app.use("/api/payments", paymentRoutes);
 
+// Default Route
 app.get("/", (req, res) => {
   res.send("SkyRoute Backend Running...");
 });
 
-module.exports = app;   
+module.exports = app;
